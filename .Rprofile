869ab2273e35f6feba77200b93be4b79eca63992
awosoga <odo.awosoga@gmail.com>
2023-08-05 21:25:37 -0400

    trying out secrets

---
+++ .Rprofile
@@ -0,0 +1,13 @@
+if (interactive()) {
+  suppressWarnings(suppressMessages(require(devtools)))
+}
+
+options(
+  "Authors@R" = utils::person(
+    "David", "Awosoga",
+    email = "odo.awosoga@gmail.com.com",
+    role = c("aut", "cre"),
+    comment = c(ORCID = "0000-0002-2741-5770")
+  ),
+  License = "MIT + file LICENSE"
+)
---
+++ R/secrets.R
@@ -0,0 +1,9 @@
+library('RPostgreSQL')
+library('RPostgres')
+data(mtcars)
+con <- dbConnect(conn = dbDriver("PostgreSQL"),
+                  dbname = Sys.getenv("DBNAME"),
+                  host = Sys.getenv("HOST"),
+                  user = Sys.getenv("USER"),
+                  password = Sys.getenv("PASSWORD"))
+dbWriteTable(con, "test", mtcars, overwrite = T)
